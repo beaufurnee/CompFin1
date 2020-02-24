35f2e4acb51761a5d8d09a4412f7019acb0f17ab
ccfelius <lottefelius@Hotmail.com>
2020-02-24 23:26:32 +0100

    American tree and plot updated

--- full_tree.py
+++ full_tree.py
@@ -7,8 +7,11 @@ Created on Mon Feb 17 11:22:32 2020
 """
 
 import math
-
-
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import quad
+from BinomialTree import black_scholes
+from BinomialTree import b_tree
 """
 
 Create tree with expected stock values
@@ -75,7 +78,8 @@ def full_tree(K, r, S0, sigma, time, steps, c = 0):
     if c == 1:
         for timestep in range(N, -1, -1):
             for node in range(timestep):
-                tree[timestep-1][node][1] = max((K - tree[timestep-1][node][0]), (math.exp((-r*dt))*((p*(tree[timestep][node][1])) + ((1-p)*(tree[timestep][min(node+1, timestep)][1])))))
+                tree[timestep-1][node][1] = max((K - tree[timestep-1][node][0]), (math.exp((-r*dt))*(
+                    (p*(tree[timestep][node][1])) + ((1-p)*(tree[timestep][min(node+1, timestep)][1])))))
 
     else:
         for timestep in range(N, -1, -1):
@@ -94,32 +98,80 @@ def full_tree(K, r, S0, sigma, time, steps, c = 0):
 
     print("Value %s option at time 0 is " % cp + str(tree[0][0][1]))
 
-    return tree
+    # return tree
+    return tree[0][0][1]
 
 
-# Example from slides (Binomial trees, slide 22)
-
-# sigma = 0.2
-# T = 12/12
-# N = 50
-# r = 0.06
-# s0 = 100
-# K = 99
-# put = 0
-
-sigma = 0.4
-T = 5/12
-N = 5
-r = 0.1
-s0 = 50
-K = 50
+sigma = 0.2
+T = 12/12
+N = 50
+r = 0.06
+s0 = 100
+K = 99
 put = 1
 
-tree = full_tree(K, r, s0, sigma, T, N, put)
+full_tree(K, r, s0, sigma, T, N, 0)
+full_tree(K, r, s0, sigma, T, N, 1)
 
-print("")
+# tree = full_tree(K, r, s0, sigma, T, N, put)
+#
+# print("")
+#
+# tis = 0
+# for timestep in tree:
+#   print(tis, timestep)
+#   tis = tis + 1
 
-tis = 0
-for timestep in tree:
-  print(tis, timestep)
-  tis = tis + 1
+
+def opt_vs_volatility(initvol, vol_max, vol_increment, K, r, s0, sigma, time, steps):
+    '''
+    Evaluate option price for a range of values for sigma (the volatility) with plot.
+    (Question I.1)
+
+    Arguments
+
+        initvol: Initial value for sigma
+        vol_max: Upper bound for sigma
+        vol_increment: Increment taken towards vol_max
+        K: strike price
+        r: interest rate
+        s0: stock price at t=0
+        sigma: volatility
+        time: maturity
+        steps: number of steps taken in the binomial tree
+    '''
+
+    option_c = []
+    option_p = []
+
+    initvol = initvol * 100
+    vol_max = vol_max * 100
+    vol_increment = vol_increment * 100
+    vol_range = range(int(initvol), int(vol_max), int(vol_increment))
+
+
+    for sigma in vol_range:
+        optionc = full_tree(K, r, s0, sigma / 100, time, steps, c=0)
+        optionp = full_tree(K, r, s0, sigma / 100, time, steps, c=1)
+        option_c.append(optionc)
+        option_p.append(optionp)
+
+    plt.figure(figsize=(8, 6))
+    plt.grid()
+    plt.ylabel('Option Price (€)', fontsize=20)
+    plt.xlabel('σ: Volatility (%)', fontsize=20)
+    plt.tick_params(axis='both', which='major', labelsize=15)
+    plt.plot(vol_range, option_c, color='yellowgreen')
+    plt.plot(vol_range, option_p, color='indianred')
+    plt.legend(['Call Option', 'Put Option'], loc='upper left')
+    plt.show()
+    plt.savefig('Images/AI-1_Opt_vsSigma', dpi=600)
+
+# Plot
+opt_vs_volatility(0.01, 6.1, 0.1, K, r, s0, sigma, T, N)
+
+# print(b_tree(K, r, s0, sigma, T, N, c = 1))
+
+
+
+
